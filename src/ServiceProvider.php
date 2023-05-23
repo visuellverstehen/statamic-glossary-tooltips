57fcca881a0cb258aa9b1b5109d44a14feed3af9
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2023-05-23 11:42:05 +0200

    chore: run vendor:publish after install

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,15 +14,19 @@ class ServiceProvider extends AddonServiceProvider
     public function boot()
     {
         parent::boot();
-        
+
+        Augmentor::addExtension('glossaryTooltips', new GlossaryTooltips());
+
+        $this->loadJsonTranslationsFrom(__DIR__ . '/../resources/lang', 'en');
+
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('glossaryTooltips.php'),
             ], 'glossaryTooltips');
         }
 
-        Augmentor::addExtension('glossaryTooltips', new GlossaryTooltips());
-
-        $this->loadJsonTranslationsFrom(__DIR__ . '/../resources/lang', 'en');
+        Statamic::afterInstalled(function ($command) {
+            $command->call('vendor:publish', ['--tag' => 'statamic-glossary-tooltips']);
+        });
     }
 }

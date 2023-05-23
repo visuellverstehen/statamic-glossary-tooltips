ebc179a4994e077dcb80801d351964dd7b804110
Flemming <mail@flemming-petersen.de>
2023-05-23 14:22:39 +0200

    chore: add types

--- src/GlossaryTooltips.php
+++ src/GlossaryTooltips.php
@@ -9,16 +9,24 @@ class GlossaryTooltips extends Mark
 {
     public static $name = 'glossaryTooltips';  // Name in markdown file
 
-    // Set non-dynamic values
-    public function addOptions()
+    /**
+     * Set non-dynamic values
+     * 
+     * @return array
+     */
+    public function addOptions(): array
     {
         return [
             'HTMLAttributes' => config('glossaryTooltips.html_attributes')
         ];
     }
 
-    // Save emitted parameters out of Vue component
-    public function addAttributes()
+    /**
+     * Save emitted parameters out of Vue component
+     * 
+     * @return array
+     */
+    public function addAttributes(): array
     {
         return [
             'type' => [],
@@ -29,8 +37,15 @@ class GlossaryTooltips extends Mark
         ];
     }
 
-    // Defines how the selected text should be marked up in the frontend
-    public function renderHTML($mark, $HTMLAttributes = [])
+    /**
+     * Defines how the selected text should be marked up in the frontend
+     * 
+     * @param $mark
+     * @param array $HTMLAttributes
+     * 
+     * @return array
+     */
+    public function renderHTML($mark, $HTMLAttributes = []): array
     {
         return [
             'dfn',
